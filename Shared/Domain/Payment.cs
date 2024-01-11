e5e96cfe2a004ea75e63e0a7443c8bdae8d18e9e
Samuel Leau <leau517@gmail.com>
2024-01-11 11:46:45 +0800

    Updated database

--- Shared/Domain/Payment.cs
+++ Shared/Domain/Payment.cs
@@ -11,7 +11,10 @@ namespace SellSwap.Shared.Domain
         public int Id { get; set; } 
         public int PaymentTypeId { get; set; }
         public virtual PaymentType? PaymentType { get; set; }
-        public int? UserId { get; set; }
+        public int UserId { get; set; }
         public virtual User? User { get; set; }
+        public int CardNumber { get; set; }
+        public DateTime CardExpireTime { get; set; }
+        public int CVV { get; set; }
     }
 }
