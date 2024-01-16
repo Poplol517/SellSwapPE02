a6f58a02fd6ab480b359b4f46d0da9f4ded5eebd
daniel <2201843F@student.tp.edu.sg>
2024-01-16 16:42:36 +0800

    category data

--- Client/Static/Endpoint.cs
+++ Client/Static/Endpoint.cs
@@ -4,6 +4,7 @@
     {
         private static readonly string Prefix = "api";
         public static readonly string ListingsEndpoint = $"{Prefix}/listings";
-        
+        public static readonly string ListingStatusEndpoint = $"{Prefix}/listingstatus";
+        public static readonly string CategorysEndpoint = $"{Prefix}/categorys";
     }
 }
